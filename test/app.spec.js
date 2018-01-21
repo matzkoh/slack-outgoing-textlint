58c4b29cb895ec5cb7c4ac89552a3481776cbb0d
matzkoh <ko.matsumoto@opt.ne.jp>
2018-01-21 16:17:50 +0900

    エラーがないときは空の JSON を返すように変更

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,20 +5,43 @@ describe('GET /', () => {
   it('200 OK', async () => {
     await request(app.callback())
       .get('/')
-      .expect(200);
+      .expect(200)
+      .expect('Content-Type', 'text/html; charset=utf-8');
   });
 });
 
 describe('POST /textlint', () => {
-  it('respond with json', async () => {
-    await request(app.callback())
-      .post('/textlint')
-      .type('form')
-      .send({
-        "text": "柱で、食べれる、じゃぱりまんが、美味しいです",
-      })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(res => expect(res.body).toHaveProperty('text'));
+  describe('lint error', () => {
+    const wrongText = '柱で、食べれる、じゃぱりまんが、美味しい';
+
+    it('respond with json', async () => {
+      await request(app.callback())
+        .post('/textlint')
+        .type('form')
+        .send({
+          text: wrongText,
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(({body}) => {
+          expect(body).toHaveProperty('link_names', true);
+          expect(body.text).toBeTruthy();
+        })
+    });
+  });
+
+  describe('no lint error', () => {
+    it('empty response', async () => {
+      await request(app.callback())
+        .post('/textlint')
+        .type('form')
+        .send({
+          text: '柱で食べるじゃぱりまんが美味しい',
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect('Content-Length', '2')
+        .expect(res => expect(res.body).toEqual({}));
+    });
   });
 });
